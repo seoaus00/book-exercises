a7ad091a60de399bda34a422aa1443503c3f9a2d
Austin Seo <austinseo@Austins-MacBook-Pro-2.local>
2020-01-30 14:25:13 -0800

    add ch11 exercise 1

--- chapter-11-exercises/exercise-1/exercise.R
+++ chapter-11-exercises/exercise-1/exercise.R
@@ -1,7 +1,12 @@
+##################
+### Chapter 11 ###
+##################
 # Exercise 1: working with data frames (review)
-
+# intstall dyplyr
+# install.packages("dplyr")
+library("dplyr")
 # Install devtools package: allows installations from GitHub
-install.packages("devtools")
+#install.packages("devtools")
 
 # Install "fueleconomy" dataset from GitHub
 devtools::install_github("hadley/fueleconomy")
@@ -11,39 +16,42 @@ library(fueleconomy)
 
 # You should now have access to the `vehicles` data frame
 # You can use `View()` to inspect it
-
+View(vehicles)
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
-
+makes <- vehicles$make
+print(makes)
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set
+length(unique(makes))
 
 
 # Filter the data set for vehicles manufactured in 1997
-
-
+vehicles_1997 <- vehicles[vehicles$year == 1997,]
+# manufactured_1997 <- filter(vehicles, year == 1997)
+# print(manufactured_1997)
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function to get a vector of indices in order by value
 # See also:
 # https://www.r-bloggers.com/r-sorting-a-data-frame-by-the-contents-of-a-column/
-
-
+ordered_vehicles <- vehicles[order(vehicles$year == 1997),]
+print(ordered_vehicles)
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
-
+vehicles$average <- (vehicles[, "hwy"] + vehicles[, "cty"]) / 2
+print(average <- vehicles$average)
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
-
+vehicles_2wd <- vehicles[vehicles$drive == "2-Wheel Drive",]
+efficient_2wd <- vehicles_2wd[vehicles_2wd$cty > 20,]
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
-
+worst_2wd <- vehicles_2wd[vehicles_2wd$hwy == min(vehicles_2wd$hwy), ]
+worst_vehicle_id <- worst_2wd[worst_2wd$hwy == min(worst_2wd$hwy), "id"]
+print(worst_vehicle_id)
 # Write a function that takes a `year_choice` and a `make_choice` as parameters, 
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
 # of that make in that year.
