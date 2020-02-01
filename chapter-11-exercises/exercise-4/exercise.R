4476f2d788910c1fd19ad110b7debe2f1427f48f
Austin Seo <austinseo@Austins-MacBook-Pro-2.local>
2020-01-31 18:49:16 -0800

    add completed ch 11 exercise 4

--- chapter-11-exercises/exercise-4/exercise.R
+++ chapter-11-exercises/exercise-4/exercise.R
@@ -2,19 +2,28 @@
 
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
-
+# install.packages("nycflights13")
+library("nycflights13")
+library("dplyr")
 
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
-
+nrow(flights)
+ncol(flights)
+colnames(flights)
+??flights
+View(flights)
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+flights <- mutate(
+  flights, 
+  delayed_in_air = (sched_arr_time - arr_time)
+)
+print(flights)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
@@ -33,10 +42,11 @@
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
-
-
+flights %>% 
+  filter(dest == "SEA") %>% 
+  summarize(avg_delayed)
 # On average, did flights to SeaTac gain or loose time?
-
+summarize(to_sea, avg_delayed = mean(delayed_in_air))
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
